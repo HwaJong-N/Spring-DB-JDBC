cfd0259f3216d50f789d97db644ab7e174f513fd
HwaJong <hwajong1016@gmail.com>
2023-01-24 20:27:31 +0900

    project_setting

---
+++ src/test/java/ghkwhd/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package ghkwhd.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
